7b6da31ba2eced55f1f08bc0f4f2f505089a36d2
Rahul Rawat <rahulrawat593@gmail.com>
2018-11-05 08:26:55 -0700

    Added login mutaion

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -2,28 +2,39 @@ import React from 'react';
 import {extendObservable} from 'mobx';
 import {observer} from 'mobx-react';
 import {Button, Container, Header, Input} from 'semantic-ui-react';
+import {gql, graphql} from 'react-apollo';
 
-export default observer(class Login extends React.Component {
+class Login extends React.Component {
     constructor(props) {
         super(props);
+
         extendObservable(this, {
             email: '',
             password: '',
         });
     }
-    //by writing like this no need to bind
-    onChange = e => {
-        const {name, value} = e.target;
-        // name = "email";
-        this.setState({[name]: value});
+    // By doing this way no need to bind
+    onSubmit = async () => {
+        const {email, password} = this;
+        const response = await this.props.mutate({
+            variables: {email, password},
+        });
+        console.log(response);
+        const {ok, token, refreshToken} = response.data.login;
+        if (ok) {
+            localStorage.setItem('token', token);
+            localStorage.setItem('refreshToken', refreshToken);
+        }
     };
 
-    onSubmit = () => {
-        const {email, password} = this;
+    onChange = e => {
+        const {name, value} = e.target;
+        this[name] = value;
     };
 
     render() {
         const {email, password} = this;
+
         return (
             <Container text>
                 <Header as="h2">Login</Header>
@@ -40,4 +51,20 @@ export default observer(class Login extends React.Component {
             </Container>
         );
     }
-});
+}
+
+const loginMutation = gql`
+  mutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      ok
+      token
+      refreshToken
+      errors {
+        path
+        message
+      }
+    }
+  }
+`;
+
+export default graphql(loginMutation)(observer(Login));
