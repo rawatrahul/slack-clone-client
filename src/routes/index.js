7266298d8f7d27fc7de795b35ea06b8d0ab7c6fd
Rahul Rawat <rahulrawat593@gmail.com>
2018-11-04 19:15:42 -0700

    Add validation for register

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import logo from '../logo.svg';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './Home';
 import Register from './Register'
