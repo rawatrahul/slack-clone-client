ec8c7cd1ac856fcbc7642651548bfeaa9aa83ac5
Rahul Rawat <rahulrawat593@gmail.com>
2018-11-20 09:31:04 -0700

    Add props to components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,16 @@
+import React from 'react';
 import styled from 'styled-components';
+import { Header } from 'semantic-ui-react';
 
-export default styled.div`
+const HeaderWrapper = styled.div`
   grid-column: 3;
   grid-row: 1;
   color: #333;
+  padding:10px;
   border-bottom: 1px solid #f2f2f2;
 `;
+export default ({ channelName }) => (
+  <HeaderWrapper>
+    <Header textAlign="left">#{channelName}</Header>
+  </HeaderWrapper>
+);
