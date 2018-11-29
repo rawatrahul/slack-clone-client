0e5dc18fc85c363c9f58c3dfbbccc0f73acaf447
Rahul Rawat <rahulrawat593@gmail.com>
2018-11-28 22:18:14 -0700

    Changelog:
    - Add dummy sidebar

--- src/routes/ViewTeam.js
+++ src/routes/ViewTeam.js
@@ -3,22 +3,25 @@ import Channels from '../components/Channels';
 import Teams from '../components/Teams';
 import Header from '../components/Header';
 import Messages from '../components/Messages';
-import Input from '../components/Input';
+import SendMessage from '../components/SendMessage';
 import AppLayout from '../components/AppLayout';
 
 export default () => (
   <AppLayout>
-    <Teams>Teams</Teams>
-    <Channels>Channels</Channels>
-    <Header>Header</Header>
+    <Teams teams={[{ id: 1, letter: 'R' }, { id: 2, letter: 'A' }]} />
+    <Channels
+      teamName="Team name"
+      username="Username"
+      channels={[{ id: 1, name: 'general' }, { id: 2, name: 'random' }]}
+      users={[{ id: 1, name: 'slackbot' }, { id: 2, name: 'user1' }]}
+    />
+    <Header channelName="general" />
     <Messages className="messages">
       <ul className="message-list">
         <li />
         <li />
       </ul>
     </Messages>
-    <Input>
-      <input type="text" placeholder="CSS Grid Layout Module" />
-    </Input>
+    <SendMessage channelName="general" />
   </AppLayout>
 );
